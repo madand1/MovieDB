0f3122e86555075c5405b1df8858f39913202ccb
Andy <148748006+madand1@users.noreply.github.com>
2024-05-17 11:59:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -44,4 +44,5 @@ def buscar_pelicula(titulo):
         return render_template("error.html", error=True)
 
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT',5000))
     app.run("0.0.0.0", debug=True)
