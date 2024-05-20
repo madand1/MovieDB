111fce595a27dc8275269f261b54a4e6e40212a5
Andy <148748006+madand1@users.noreply.github.com>
2024-05-20 10:16:33 +0200

    Update tmdb.py

--- tmdb.py
+++ tmdb.py
@@ -1,23 +1,50 @@
 import requests
 
-# URL base para la API de TheMovieDB
-url_base = "https://api.themoviedb.org/3/"
-# Definir la clave de la API y el código de país directamente en el código
-api_key = "1b5e4c5722010f132052504f17ae4c6c"
-country_code = "ES"
-def buscar_peliculas(titulo):
-    url = f'{url_base}/search/movie'
-    params = {
-        'api_key': api_key,
-        'query': titulo,
-    }
-    response = requests.get(url, params=params)
-    return response.json().get('results', [])
+API_KEY = api_key = "1b5e4c5722010f132052504f17ae4c6c"
+BASE_URL = 'https://api.themoviedb.org/3'
 
-def obtener_detalle_pelicula(id_pelicula):
-    url = f'{url_base}/movie/{id_pelicula}'
+def buscar_peliculas(query):
+    url = f'{BASE_URL}/search/movie'
     params = {
-        'api_key': api_key,
+        'api_key': API_KEY,
+        'query': query,
+        'language': 'es-ES'
     }
     response = requests.get(url, params=params)
-    return response.json()
+    if response.status_code == 200:
+        return response.json().get('results', [])
+    return []
+
+def obtener_detalle_pelicula(id):
+    url = f'{BASE_URL}/movie/{id}'
+    params = {
+        'api_key': API_KEY,
+        'language': 'es-ES'
+    }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        return response.json()
+    return None
+
+def buscar_actores(query):
+    url = f'{BASE_URL}/search/person'
+    params = {
+        'api_key': API_KEY,
+        'query': query,
+        'language': 'es-ES'
+    }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        return response.json().get('results', [])
+    return []
+
+def obtener_detalle_actor(id):
+    url = f'{BASE_URL}/person/{id}'
+    params = {
+        'api_key': API_KEY,
+        'language': 'es-ES'
+    }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        return response.json()
+    return None
