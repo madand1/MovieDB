ccf6520257f78fdf35a8f1d00ec9221ae8e97fe1
Andy <148748006+madand1@users.noreply.github.com>
2024-05-21 08:43:48 +0200

    Update tmdb.py

--- tmdb.py
+++ tmdb.py
@@ -2,10 +2,10 @@ import requests
 
 BASE_URL = 'https://api.themoviedb.org/3'
 
-def buscar_peliculas(query):
+def buscar_peliculas(query, api_key):
     url = f'{BASE_URL}/search/movie'
     params = {
-        'api_key': API_KEY,
+        'api_key': api_key,
         'query': query,
         'language': 'es-ES'
     }
@@ -14,10 +14,10 @@ def buscar_peliculas(query):
         return response.json().get('results', [])
     return []
 
-def obtener_detalle_pelicula(id):
+def obtener_detalle_pelicula(id, api_key):
     url = f'{BASE_URL}/movie/{id}'
     params = {
-        'api_key': API_KEY,
+        'api_key': api_key,
         'language': 'es-ES'
     }
     response = requests.get(url, params=params)
@@ -25,10 +25,10 @@ def obtener_detalle_pelicula(id):
         return response.json()
     return None
 
-def buscar_actores(query):
+def buscar_actores(query, api_key):
     url = f'{BASE_URL}/search/person'
     params = {
-        'api_key': API_KEY,
+        'api_key': api_key,
         'query': query,
         'language': 'es-ES'
     }
@@ -37,10 +37,10 @@ def buscar_actores(query):
         return response.json().get('results', [])
     return []
 
-def obtener_detalle_actor(id):
+def obtener_detalle_actor(id, api_key):
     url = f'{BASE_URL}/person/{id}'
     params = {
-        'api_key': API_KEY,
+        'api_key': api_key,
         'language': 'es-ES'
     }
     response = requests.get(url, params=params)
